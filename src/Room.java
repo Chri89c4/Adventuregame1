4204f9ec08abd11a4f8237f0f66f0a7eeffb99a4
Chris <chri89c4@stud.kea.dk>
2021-09-30 10:03:45 +0200

    Tilføjelse af referencer til rum

--- src/Room.java
+++ src/Room.java
@@ -1,3 +1,15 @@
 public class Room {
 
+  Room r1 = new Room();
+  Room r2 = new Room();
+  Room r3 = new Room();
+  Room r4 = new Room();
+  Room r5 = new Room();
+  Room r6 = new Room();
+  Room r7 = new Room();
+  Room r8 = new Room();
+  Room r9 = new Room();
+
+
+
 }
