77558d7252c3222e829c6d4ba5878effde2d60a6
tobias juul Rasmussen <tobiasras.job@gmail.com>
2021-09-30 09:22:37 +0200

    Lavede en input metode

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Adventure {
   public static void main(String[] args) {
 
   }
+
+
+
+
+  public static String getInput(){
+    Scanner scanner = new Scanner(System.in);
+
+    return scanner.nextLine();
+  }
+
 }
