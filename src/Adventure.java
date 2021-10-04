8f9ef7b36ef229df536cf379ca697dd045bf53ac
tobias juul Rasmussen <tobiasras.job@gmail.com>
2021-10-04 10:07:17 +0200

    Map Opgave

--- src/Adventure.java
+++ src/Adventure.java
@@ -4,94 +4,54 @@ public class Adventure {
     public static void main(String[] args) {
 
         Adventure a = new Adventure();
-
-        Room r1 = new Room("Rum1", "Dette er rum 1");
-        Room r2 = new Room("Rum2", "Dette er rum 2");
-        Room r3 = new Room("Rum3", "Dette er rum 3");
-        Room r4 = new Room("Rum4", "Dette er rum 4");
-        Room r5 = new Room("Rum5", "Dette er rum 5");
-        Room r6 = new Room("Rum6", "Dette er rum 6");
-        Room r7 = new Room("Rum7", "Dette er rum 7");
-        Room r8 = new Room("Rum8", "Dette er rum 8");
-        Room r9 = new Room("Rum9", "Dette er rum 9");
-
-        r1.setSouth(r4);
-        r1.setEast(r2);
-
-        r2.setWest(r1);
-        r2.setEast(r3);
-
-        r3.setWest(r2);
-        r3.setSouth(r6);
-
-        r4.setNorth(r1);
-        r4.setSouth(r7);
-
-        r5.setSouth(r8);
-
-        r6.setNorth(r3);
-        r6.setSouth(r9);
-
-        r7.setNorth(r4);
-        r7.setEast(r8);
-
-        r8.setNorth(r5);
-        r8.setWest(r7);
-        r8.setEast(r9);
-
+        Map map = new Map();
 
         // Start rummet
-        Room currentRoom = r1;
-
-        System.out.println("Du befinder dig i "+ currentRoom );
+        System.out.println("Du befinder dig i "+ Map.currentRoom );
 
         System.out.println("For at færdes i grotten kan bruge commandoerne:\n");
+
         a.help();
 
 
         System.out.println("\nHVAD VIL DU?");
 
-        while (true) {
+        do {
             String userInput = a.getInput();
 
             if (userInput.equals("go north")) {
-                if (currentRoom.getNorth() != null) {
-                    currentRoom = currentRoom.getNorth();
-                    System.out.println("Du er nu i rum " + currentRoom);
+                if (Map.currentRoom.getNorth() != null) {
+                    Map.currentRoom = Map.currentRoom.getNorth();
+                    System.out.println("Du er nu i rum " + Map.currentRoom);
                 } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + currentRoom);
+                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
             }
 
             if (userInput.equals("go east")) {
-                if (currentRoom.getEast() != null) {
-                    currentRoom = currentRoom.getEast();
-                    System.out.println("Du er nu i rum " + currentRoom);
+                if (Map.currentRoom.getEast() != null) {
+                    Map.currentRoom = Map.currentRoom.getEast();
+                    System.out.println("Du er nu i rum " + Map.currentRoom);
                 } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + currentRoom);
+                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
             }
 
             if (userInput.equals("go west")) {
-                if (currentRoom.getWest() != null) {
-                    currentRoom = currentRoom.getWest();
-                    System.out.println("Du er nu i rum " + currentRoom);
+                if (Map.currentRoom.getWest() != null) {
+                    Map.currentRoom = Map.currentRoom.getWest();
+                    System.out.println("Du er nu i rum " + Map.currentRoom);
                 } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + currentRoom);
+                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
             }
 
             if (userInput.equals("go south")) {
-                if (currentRoom.getSouth() != null) {
-                    currentRoom = currentRoom.getSouth();
-                    System.out.println("Du er nu i rum " + currentRoom);
+                if (Map.currentRoom.getSouth() != null) {
+                    Map.currentRoom = Map.currentRoom.getSouth();
+                    System.out.println("Du er nu i rum " + Map.currentRoom);
                 } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + currentRoom);
+                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
 
             }
-        }
-    }
-
-    public void map(){
-
-
+        } while (true);
     }
 
     public String getInput() {
--- src/Map.java
+++ src/Map.java
@@ -1,16 +1,20 @@
 public class Map {
-    private String currentRoom;
+    static Room currentRoom;
+
+    Room r1 = new Room("Rum1", "Dette er rum 1");
+    Room r2 = new Room("Rum2", "Dette er rum 2");
+    Room r3 = new Room("Rum3", "Dette er rum 3");
+    Room r4 = new Room("Rum4", "Dette er rum 4");
+    Room r5 = new Room("Rum5", "Dette er rum 5");
+    Room r6 = new Room("Rum6", "Dette er rum 6");
+    Room r7 = new Room("Rum7", "Dette er rum 7");
+    Room r8 = new Room("Rum8", "Dette er rum 8");
+    Room r9 = new Room("Rum9", "Dette er rum 9");
+
 
     Map(){
-        Room r1 = new Room("Rum1", "Dette er rum 1");
-        Room r2 = new Room("Rum2", "Dette er rum 2");
-        Room r3 = new Room("Rum3", "Dette er rum 3");
-        Room r4 = new Room("Rum4", "Dette er rum 4");
-        Room r5 = new Room("Rum5", "Dette er rum 5");
-        Room r6 = new Room("Rum6", "Dette er rum 6");
-        Room r7 = new Room("Rum7", "Dette er rum 7");
-        Room r8 = new Room("Rum8", "Dette er rum 8");
-        Room r9 = new Room("Rum9", "Dette er rum 9");
+        // sætter current room til rum 1
+        currentRoom = r1;
 
         r1.setSouth(r4);
         r1.setEast(r2);
@@ -35,6 +39,9 @@ public class Map {
         r8.setNorth(r5);
         r8.setWest(r7);
         r8.setEast(r9);
+
+        r9.setNorth(r6);
+        r9.setWest(r8);
 
     }
 }
