5dbfe4764fecbe98f56477dc17ff33879fd94e6a
Chris <chri89c4@stud.kea.dk>
2021-10-04 11:40:03 +0200

    added RoomDrscription

--- src/Description.java
+++ src/Description.java
@@ -1,4 +1,19 @@
 public class Description {
-    private String[] adjectiveListScarry = { "shadowy", "narrow", "dark", "moist", "dimm", "endless",
-            "muddy", "glommy", "uncomftable", "silent", "sinister", "intense" };
+
+    //System.out.println(currentroom + "- You find yourself in a very" + random adjectiveListScarry + "room. " + random secondDescription);
+
+    private String[] adjectiveListScarry = {"shadowy", "dark", "moist", "dimm", "endless",
+        "muddy", "glommy", "uncomftable", "silent", "sinister", "intense", "horrifying", "fearful", "fearsome"};
+
+    private String [] secondRoomDescription = {
+        "Blood is dripping down from the sealing of the cave.",
+        "The floor is covered in bones from some sort of animal creature.",
+        "On the walls you'll find a multiple of handprints made out of blood.",
+        "Along the right side of the tunnel you'll find multiple body parts from what seems to be a human.",
+        "A horrific smell Is filling the room...   it’s the smell of burned skin.",
+        "The walls are full of pointy needles, so becarefull to not bump into them.",
+        "On one of the walls you'll find a word writtin in blood saying... RUN!",
+        "There is a never ending hole where the floor should've been, the only way to get across the room is to walk on a slim piece of wood. Be careful!"
+    };
 }
+
