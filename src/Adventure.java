14c4bd6ed9500b5582d39e333ee2881099413cb8
RenasAli <rena0506@stud.kea.dk>
2021-10-04 11:56:03 +0200

    Move all commands to Commands class

--- src/Adventure.java
+++ src/Adventure.java
@@ -16,62 +16,8 @@ public class Adventure {
 
 
         System.out.println("\nHVAD VIL DU?");
-        //Commands.getAllInput();
+
         Commands commands= new Commands();
-
-       /* do {
-            String userInput = a.getInput();
-
-            if (userInput.equals("go north")) {
-                if (Map.currentRoom.getNorth() != null) {
-                    Map.currentRoom = Map.currentRoom.getNorth();
-                    System.out.println("Du er nu i rum " + Map.currentRoom);
-                } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
-            }
-
-            if (userInput.equals("go east")) {
-                if (Map.currentRoom.getEast() != null) {
-                    Map.currentRoom = Map.currentRoom.getEast();
-                    System.out.println("Du er nu i rum " + Map.currentRoom);
-                } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
-            }
-
-            if (userInput.equals("go west")) {
-                if (Map.currentRoom.getWest() != null) {
-                    Map.currentRoom = Map.currentRoom.getWest();
-                    System.out.println("Du er nu i rum " + Map.currentRoom);
-                } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
-            }
-
-            if (userInput.equals("go south")) {
-                if (Map.currentRoom.getSouth() != null) {
-                    Map.currentRoom = Map.currentRoom.getSouth();
-                    System.out.println("Du er nu i rum " + Map.currentRoom);
-                } else
-                    System.out.println("you bumbed in a wall \n" + "you are still in " + Map.currentRoom);
-
-            }
-            userInput = Commands.getAllInput();
-            if (userInput.equals("exit")) {
-                System.out.println("Are you sure");
-                //gamesRunning = false;
-                if (userInput.equals("yes")) {
-                    System.out.println("The End...!");
-                    gamesRunning = false;
-
-                }
-                if (userInput.equals("no")) {
-                    System.out.println("Good");
-                    System.out.println("You can continue your game");
-                    gamesRunning = true;
-                }
-            }
-
-        } while (gamesRunning);
-        */
 
 
     }
